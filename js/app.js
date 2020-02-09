d7e8e27bac9d04b4a435053e81b61d74fadec70f
Ralph Tamayo <ralphtamayo@creatinginfo.com>
2020-02-09 11:14:48 +0800

    Set-up player objects

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 $(document).ready(() => {
+	let players = [];
+
 	$('#mainMenu').show();
 	$('#inGame').hide();
 
 	$('#newGameBtn').on('click', () => {
 		$('#mainMenu').hide();
 		$('#inGame').show();
+
+		players.push(
+			new Player('Player 1'),
+			new Player('Player 2')
+		);
+
+		players.forEach(player => {
+			let playerHtml = generatePlayerHtml(player);
+
+			$('#playersContainer').append(playerHtml);
+		});
 	});
 
 	$('#rollDiceBtn').on('click', () => {
@@ -16,6 +29,24 @@ $(document).ready(() => {
 		});
 	});
 });
+
+function Player(name) {
+	this.name = name;
+	this.totalScore = 0;
+	this.currentScore = 0;
+}
+
+let generatePlayerHtml = (player) => `
+	<div class="col-6">
+		<div class="jumbotron m-0">
+			<h4 class="player-name">${ player.name }</h4>
+			<h1 class="display-1 score">${ player.totalScore }</h1>
+			<hr class="my-4">
+			<p>Current:</p>
+			<p class="lead">${ player.currentScore }</p>
+		</div>
+	</div>
+`;
 
 let rollDice = () => {
 	return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
