7bea804bc2eb9cab6014087c8cea24b9cd8112b8
Ralph Tamayo <ralphtamayo@creatinginfo.com>
2020-02-09 10:42:55 +0800

    Roll dice when the button is clicked

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(() => {
 	$('#mainMenu').show();
 	$('#inGame').hide();
 
@@ -6,4 +6,17 @@ $(document).ready(function () {
 		$('#mainMenu').hide();
 		$('#inGame').show();
 	});
+
+	$('#rollDiceBtn').on('click', () => {
+		$('.die-list').each((i, die) => {
+			die.dataset.roll = rollDice();
+
+			die.classList.toggle('even-roll');
+			die.classList.toggle('odd-roll');
+		});
+	});
 });
+
+let rollDice = () => {
+	return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+}
